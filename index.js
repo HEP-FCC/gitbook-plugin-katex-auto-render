f8cfea1dd5de196eab57aa30efd36daaab770330
Alex Pearce <alex@alexpearce.me>
2017-05-05 07:20:15 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  website: {
+    assets: "./assets",
+    css: [
+      "katex.min.css"
+    ],
+    js: [
+      "katex.min.js",
+      "auto-render.min.js",
+      "enable-auto-render.js"
+    ]
+  }
+};
